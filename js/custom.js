c955c6a598f4cca4d0de1f0d8047706a092e1102
Morten Medici <mortenmedici@gmail.com>
2019-03-17 13:33:44 +0100

    change to version 2.0

---
+++ js/custom.js
@@ -0,0 +1,36 @@
+  /*-------------------------------------------------------------------------------
+    PRE LOADER
+  -------------------------------------------------------------------------------*/
+
+  $(window).load(function(){
+    $('.preloader').fadeOut(800); // set duration in brackets    
+  });
+
+
+  /*-------------------------------------------------------------------------------
+    jQuery Parallax
+  -------------------------------------------------------------------------------*/
+
+    function initParallax() {
+    $('#home').parallax("50%", 0.3);
+
+  }
+  initParallax();
+
+
+  /* Back top
+  -----------------------------------------------*/
+  var btn = $('.go-top');
+
+  $(window).scroll(function() {
+    if ($(window).scrollTop() > 300) {
+      btn.addClass('show');
+    } else {
+      btn.removeClass('show');
+    }
+  });
+
+  btn.on('click', function(e) {
+    e.preventDefault();
+    $('html, body').animate({scrollTop:0}, '300');
+  });
