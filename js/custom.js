6ac2c154436611e9699b56f974e0b924b0d222f3
Morten Medici <mortenmedici@gmail.com>
2019-09-05 14:14:24 +0200

    update programme

--- js/custom.js
+++ js/custom.js
@@ -1,24 +1,38 @@
-  /*-------------------------------------------------------------------------------
-    PRE LOADER
-  -------------------------------------------------------------------------------*/
+/*-------------------------------------------------------------------------------
+PRE LOADER
+-------------------------------------------------------------------------------*/
 
-  $(window).load(function(){
+$(window).load(function(){
     $('.preloader').fadeOut(800); // set duration in brackets    
-  });
+});
 
-  /* Back top
-  -----------------------------------------------*/
-  var btn = $('.go-top');
 
-  $(window).scroll(function() {
+/* Back top
+-----------------------------------------------*/
+var btn = $('.go-top');
+
+$(window).scroll(function() {
     if ($(window).scrollTop() > 300) {
-      btn.addClass('show');
+        btn.addClass('show');
     } else {
-      btn.removeClass('show');
+        btn.removeClass('show');
     }
-  });
+});
 
-  btn.on('click', function(e) {
+btn.on('click', function(e) {
     e.preventDefault();
     $('html, body').animate({scrollTop:0}, '300');
-  });
+});
+
+
+/* In order to fix stuck parallax when opening closing collapsibles 
+-------------------------------------------------*/
+function helpParallax() {
+    jQuery(window).trigger('resize').trigger('scroll');
+}
+
+/* find all 'details' on page and add the attribute to chekc ontoggles */
+var matches = document.querySelectorAll('details');
+for (var i = 0; i < matches.length; i++) {
+    matches[i].setAttribute('ontoggle', 'helpParallax()');
+}
